e88058aa14a51d5d497afcb41202e32b0f6b7cb1
TaeWook Park <155722702+ld5ehom@users.noreply.github.com>
2024-07-01 14:28:28 -0700

    Delete backend directory

--- backend/src/main/java/com.store.backend/repository/GoodsItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.store.backend.repository;
-
-import com.store.backend.entity.GoodsItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-
-public interface GoodsItemRepository extends JpaRepository<GoodsItem, Long> {}
