f00018810ba4ee4677b774da426f66ddcf23b73b
TaeWook Park <155722702+ld5ehom@users.noreply.github.com>
2024-07-12 20:09:31 -0700

    rebuild ucla store project

---
+++ src/main/java/com/store/backend/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.store.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping(value = "/")
+    public String index(){
+        return "Welcome to the UCLA Store!";
+    }
+}
+//http://localhost:8080/
