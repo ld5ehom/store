a8b5f5083b86ffc8b488d2d0dd324b385df887cc
TaeWook Park <155722702+ld5ehom@users.noreply.github.com>
2024-07-12 18:01:52 -0700

    Delete backend directory

--- backend/src/main/java/com/store/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.store.backend.repository;
-
-import com.store.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
