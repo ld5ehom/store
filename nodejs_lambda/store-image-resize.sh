f7a34ceeb09350795056ce461e504904c76322b5
TaeWook Park <155722702+ld5ehom@users.noreply.github.com>
2024-07-12 17:41:38 -0700

    Delete nodejs_lambda directory

--- nodejs_lambda/store-image-resize.sh
+++
@@ -1,5 +0,0 @@
-npm install
-zip -r storeImageResize.zip .
-aws lambda update-function-configuration --function-name store-image-resize --handler storeImageResize.handler --timeout 60 --description aws:states:opt-out
-aws lambda update-function-code --function-name store-image-resize --zip-file fileb://storeImageResize.zip --no-paginate --no-cli-pager
-rm storeImageResize.zip
